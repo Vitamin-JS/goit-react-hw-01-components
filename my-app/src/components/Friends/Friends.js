4599040bd3ca1b314708ef8dc116af29371b5e30
Vitamin-JS <Vpopchenkov@gmail.com>
2020-11-29 01:18:23 +0200

    Add Profile Task structure

---
+++ my-app/src/components/Friends/Friends.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import s from './Friend.modele.css';
+
+const profileOptions = { name, tag, location, avatar, stats };
+
+function Profile(profileOptions) { };
+
+Friends.propTypes = {
+
+};
+
+export default Profile;
+
+// name — имя пользователя
+// tag — тег в социальной сети без @
+// location — город и страна
+// avatar — url на изображение
+// stats — объект с информацией об активности
